d532641081f30c9c2c4b20965487464629ae2935
Devon Bull <dbull@live.com>
2017-04-08 23:39:01 -0600

    Background img up

---
+++ src/Profile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Profile.css';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div className="page-header">
+        <img className="background-img" src="" alt="profile picture"/>
+      </div>
+    )
+  }
+}
+
+export default Profile;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Profile from './Profile';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Profile />,
   document.getElementById('root')
 );
